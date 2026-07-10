b8e7fccf52819253d177691fe9f1e32d2997d07e
shirinyannver31-ux <shirinyannver31@gmail.com>
2026-07-10 10:17:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// VedaVPN Service Worker — v3.2.2
-const CACHE_NAME = 'vedavpn-v3-2-2';
+// VedaVPN Service Worker — v3.3.0
+const CACHE_NAME = 'vedavpn-v3-3-0';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
